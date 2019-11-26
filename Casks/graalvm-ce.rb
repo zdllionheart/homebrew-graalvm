de5cdc5e74cb83a7baa4c688c956eeea0ee62bb4
zdllionheart <zdllionheart@126.com>
2019-11-26 21:50:05 +0800

    Update graalvm-ce.rb

--- Casks/graalvm-ce.rb
+++ Casks/graalvm-ce.rb
@@ -1,7 +1,6 @@
 cask 'graalvm-ce' do
-  version '19.2.0'
-  sha256 '54bd7f7398ee0fb2d1c9eb8a939a5ccb05166cb9c544bc700020fc37dc661483'
-
+  version '19.3.0'
+  
   # github.com/oracle/graal was verified as official when first introduced to the cask
   url "https://github.com/oracle/graal/releases/download/vm-#{version.downcase}/graalvm-ce-darwin-amd64-#{version.downcase}.tar.gz"
   appcast 'https://github.com/oracle/graal/releases.atom'
